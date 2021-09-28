0d75d085d540e6f796504cbdb25d123d33f362e3
Athul Sabu <88123015+athulcoder@users.noreply.github.com>
2021-09-28 17:56:55 +0530

    Update Dictionaries.py

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,14 +1,14 @@
 display = {
           
-          "Name":"Athul Sabu",
-          "Age": 14 ,
-          "Father":"Sabu",
-          "Mother":"Usha Antony"
+          "Name":"person-name",
+          "Age": age of the person,
+          "Father":"Name of Father ",
+          "Mother":"Name of Mother "
 }
 
 while True:
 	output= input("What U Want : ")
 	if output in display:
 		print(display[output])
-	elif output=="n":
+	elif output=="exit":
 		break
