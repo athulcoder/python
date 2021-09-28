4fe2f7687a376bd374769a0bf7c0f7fc06131ac3
Athul Sabu <88123015+athulcoder@users.noreply.github.com>
2021-09-28 18:00:55 +0530

    Add files via upload

---
+++ Wikipedia.py
@@ -0,0 +1,26 @@
+import wikipedia
+
+
+def search(query):
+    result = wikipedia.summary(query, sentences=5)
+    print(result)
+
+
+def yes(opinion):
+    if opinion == "y":
+        query = input("Search: ")
+        search(query)
+    else:
+        pass
+
+
+query = input("Search: ")
+search(query)
+while True:
+    reply = input("\n Would you like to search another query ? [y] or [n]: ")
+    yes(reply)
+    if reply == "n":
+        print("\n Thank You for using")
+        break
+    else:
+        print("Say yes or no.")
