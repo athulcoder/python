a76c86f1963d4e20ba244eac79dcbe0ac98e29a9
Athul Sabu <88123015+athulcoder@users.noreply.github.com>
2021-10-23 05:27:01 +0530

    Update Guess_number_random.py

--- Guess_number_random.py
+++ Guess_number_random.py
@@ -4,7 +4,7 @@ import random
 #defined a funtion call main
 def main():
 	# defined a variable call number to store the randow number between 1 to 10
-	number = int(11*(random.random()))
+	secret_number = int(11*(random.random()))
   # starts a while loop which is expressed as true	
 	while True:
 	# defined an input varible call Guess to store the integer  
@@ -12,8 +12,8 @@ def main():
 # if the Guessed number is equal to the number stored in the varible call number ,it will print "you win.." and then it will ask the user  if they would like to continue the Game..
 # To enter the input (y or n) of the user a varible call again is defined 
 
-		if Guess == number:
-			print("You Win ....")
+		if Guess == secret_number:
+                        print("You Win ....")
 			again = input("\n   Do You like to continue[y] or [n] : ")
 # if the input is "y" then it will continue the Game and if the input is "n" then it will break the loop and end the Game 
 			if again =="y":
@@ -22,9 +22,9 @@ def main():
 				break
 			else :
 				print('sorry I didn\'t get you ..')
-		elif  Guess > number :
+		elif  Guess > secret_number :
 			print("The Number is less  than this Number  ")     
-		elif Guess < number:
+		elif Guess < secret_number:
 			print("The Number is more then this number  ")
 			
 			
