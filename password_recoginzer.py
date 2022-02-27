f03de61b0cd48d2d801c2c32a24c35021a37915a
Athul Sabu <88123015+athulcoder@users.noreply.github.com>
2022-02-27 16:06:37 +0530

    Add files via upload

---
+++ password_recoginzer.py
@@ -0,0 +1,15 @@
+from random import *
+
+password = input ("\n Enter your password : ")
+letters = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+guess = ""
+
+
+while guess != password :
+    guess = ""
+    for letter in range(len(password)):
+        guess_letter =  letters[randint(0,25)]
+        guess = str(guess_letter) + str(guess)
+        print(guess)
+
+print("\n your password is ", guess)
